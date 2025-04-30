6c79a8ef791dcfc72b8ebc5edd8330a9968f699d
ValenFar <maxy.far@gmail.com>
2025-04-30 01:35:59 -0300

    Implementacion de App.jsx y main.jsx MaximoValentinFarfanAntileo

--- assets/App.jsx
+++ assets/App.jsx
@@ -1,9 +1,34 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState } from 'react';
+import FormularioEvento from './src/components/FormularioEvento';
+import './css/index.css';
+import ListaProductos from './src/components/ListaProductos';
 
-function App() {
-  
-  
+const App = () => {
+    const [eventos, setEventos] = useState([]);
+
+    const agregarEvento = (nuevoEvento) => {
+        setEventos([...eventos, nuevoEvento]);
+    };
+
+    return (
+        <div className='contenedor'>
+            <h1>Lista de Eventos</h1>
+            <FormularioEvento Agregar={agregarEvento} />
+        <div className="lista-eventos">
+            {eventos.map((evento, index) => (
+            <input
+                key={index}
+                type="text"
+                value={evento.titulo}
+                readOnly // Hace que el input no sea editable
+            />
+        ))}
+        </div>
+        <ListaProductos />
+    </div>
+);
 }
 
-export default App
+
+
+export default App;
