384c4d1a5a9933b06fbcfdeadb1ea5cd21686890
ValenFar <maxy.far@gmail.com>
2025-05-06 03:40:39 -0300

    Se agregaron los nuevos complementos a App.jsx MaximoValentinFarfanAntileo

--- assets/App.jsx
+++ assets/App.jsx
@@ -2,33 +2,51 @@ import React, { useState } from 'react';
 import FormularioEvento from './src/components/FormularioEvento';
 import './css/index.css';
 import ListaProductos from './src/components/ListaProductos';
+import ListaElementos from './src/components/ListaElementos';
 
 const App = () => {
     const [eventos, setEventos] = useState([]);
 
-    const agregarEvento = (nuevoEvento) => {
-        setEventos([...eventos, nuevoEvento]);
-    };
+    const agregarEvento = (evento) => {
+        setEventos([...eventos, { ...evento, id: Date.now() }])
+    }
+    // Función para alternar el estado de completado de un evento
+    const completarEvento = (id) => {
+        setEventos(
+            eventos.map((evento) =>
+                evento.id === id ? { ...evento, completado: !evento.completado } : evento
+            )
+        )
+    }
+  // Función para editar un evento
+    const editarEvento = (id, nuevosDatos) => {
+        setEventos(
+            eventos.map((evento) =>
+                evento.id === id ? { ...evento, ...nuevosDatos } : evento
+            )
+        )
+    }
+
+    // Función para eliminar un evento
+    const eliminarEvento = (id) => {
+        setEventos(eventos.filter((evento) => evento.id !== id))
+    }
+
 
     return (
-        <div className='contenedor'>
-            <h1>Lista de Eventos</h1>
+        <div className="contenedor">
+            <h1>Administrador de Tareas</h1>
             <FormularioEvento Agregar={agregarEvento} />
-        <div className="lista-eventos">
-            {eventos.map((evento, index) => (
-            <input
-                key={index}
-                type="text"
-                value={evento.titulo}
-                readOnly // Hace que el input no sea editable
+            <ListaElementos
+                eventos={eventos}
+                completarEvento={completarEvento}
+                editarEvento={editarEvento}
+                eliminarEvento={eliminarEvento}
             />
-        ))}
+            <ListaProductos />
         </div>
-        <ListaProductos />
-    </div>
-);
+        
+    )
 }
-
-
 
 export default App;
